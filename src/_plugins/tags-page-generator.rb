b350d342c5e3c1eb9cedbd8102b9f44e31a36f84
Jeferson Amorim <amorimjj@gmail.com>
2023-05-16 12:15:33 -0300

    Add other tags feature

--- src/_plugins/tags-page-generator.rb
+++ src/_plugins/tags-page-generator.rb
@@ -1,12 +1,67 @@
+require_relative "url-filter"
+
+class TagHandler
+
+  attr_accessor :tags
+
+  def initialize
+    @tags = {}
+  end
+
+  def check(posts)
+    posts.each do |post|
+      tag = tag_sanitize(post.data['tag'])
+      add(post.data['tag'], post)
+      if post['others_tags']
+        others_tags = post['others_tags'].split('|')
+        others_tags.each do |tag|
+          add(tag, post)
+        end
+      end
+    end
+  end
+
+  private
+
+  def add(tag, post)
+    id = tag_sanitize(tag)
+    if !@tags[id]
+      @tags[id] = { 'name' => tag, 'posts' => [post] }
+    elsif !@tags[id]['posts'].find { |_post| _post.data['title'] == post.data['title'] }
+      @tags[id]['posts'].push(post)
+    end
+  end
+end
+
+module Jekyll
+  module TagsFilter
+    def tags_from_site(site)
+      handler = TagHandler.new
+      handler.check(site.posts)
+      handler.tags.values
+    end
+
+    def tagname_sanitize(tag)
+      "##{tag.downcase.gsub(/\s/, "")}"
+    end
+  end
+end
+
 module TagsPageGeneratorPlugin
     class TagsPageGenerator < Jekyll::Generator
       safe true
+
+      def create_page(site, tag, posts)
+        if ! site.pages.find { |page| page.name == "tags/#{tag_sanitize(tag)}.html" }
+          site.pages << TagPage.new(site, tag, posts)
+        end
+      end
   
       def generate(site)
-        site.tags.each do |tag, posts|
-            if ! site.pages.find { |page| page.name == "tags/#{tag}.html" }
-                site.pages << TagPage.new(site, tag, posts)
-            end
+        handler = TagHandler.new
+        handler.check(site.posts.docs)
+        handler.tags.each do |name, tag|
+          create_page(site, tag['name'], tag['posts'])
         end
       end
     end
@@ -19,9 +74,9 @@ module TagsPageGeneratorPlugin
         @dir  = "tags"         # the directory the page will reside in.
   
         # All pages have the same filename, so define attributes straight away.
-        @basename = tag      # filename without the extension.
-        @ext      = '.html'      # the extension.
-        @name     = "#{tag}.html" # basically @basename + @ext.
+        @basename = tag_sanitize(tag)      # filename without the extension.
+        @ext      = '.html'                # the extension.
+        @name     = "#{@basename}.html"          # basically @basename + @ext.
 
         info = site.config['tags_config'].find { |search| search['name'] == tag }
   
@@ -29,11 +84,11 @@ module TagsPageGeneratorPlugin
         # This allows accessing the list in a template via `page.linked_docs`.
         @data = {
           'posts' => posts,
-          'title' => info ? info['title'] : tag,
+          'title' => info ? info['title'] : tag.capitalize,
           'tag' => tag
         }
 
-        @content = info ? info['description'] : ''
+        @content = info ? info['description'] : 'Aprender a programar pode ser um desafio. Me siga que eu vou te ajudar nesse processo. Programar mudou a minha vida e tenho certeza que pode mudar a sua!'
   
         # Look up front matter defaults scoped to type `tags`, if given key
         # doesn't exist in the `data` hash.
@@ -53,3 +108,5 @@ module TagsPageGeneratorPlugin
       end
     end
 end
+
+Liquid::Template.register_filter(Jekyll::TagsFilter)
